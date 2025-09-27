1bcf866b4ce40412e6fd653beaaa55fccaf916ae
Will Dev <tu-usuario-github@users.noreply.github.com>
2025-09-26 20:58:33 -0500

    Configurado puerto dinámico para Railway

--- api-autenticacion/src/server.js
+++ api-autenticacion/src/server.js
@@ -9,7 +9,7 @@ const rutaEmpleados = require('./routes/empleados.routes');
 const ticketsRoutes = require('./routes/tickets.routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
